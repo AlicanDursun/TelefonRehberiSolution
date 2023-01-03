acb0dc7000860f730837186dfa6c9e7336482ac7
AlicanDursun <alicangdursun@gmail.com>
2023-01-03 22:23:57 +0300

    EventBus,RabbitMQ Configuration

---
+++ Src/BuildingBlocks/EventBus/EventBus.Base/SubManagers/InMemoryEventBusSubscriptionManager.cs
@@ -0,0 +1,126 @@
+﻿using EventBus.Base.Abstraction;
+using EventBus.Base.Events;
+
+
+namespace EventBus.Base.SubManagers
+{
+    public class InMemoryEventBusSubscriptionManager : IEventBusSubscriptionManager
+    {
+        //Handlerları tutabilmek için bir dictionary
+        private readonly Dictionary<string, List<SubscriptionInfo>> _handlers;
+        private readonly List<Type> _eventTypes;
+
+        public event EventHandler<string>? OnEventRemoved;
+
+        //eventnamegetterlarına göre que oluşturmak için order completed integration event gibi generic olarak integration event koymak
+        private readonly Func<string, string> _eventNameGetter;
+        public InMemoryEventBusSubscriptionManager(Func<string, string> eventNameGetter)
+        {
+            _handlers = new Dictionary<string, List<SubscriptionInfo>>();
+            _eventTypes = new List<Type>();
+            _eventNameGetter = eventNameGetter;
+        }
+
+        //handlerda key olup olmadığına bakılacak
+        public bool IsEmpty => !_handlers.Keys.Any();
+        //handler clear etme
+        public void Clear() => _handlers.Clear();
+
+
+        public void AddSubscription<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>
+        {
+            var eventName = GetEventKey<T>();
+            AddSubscription(typeof(TH), eventName);
+            if (!_eventTypes.Contains(typeof(T)))
+            {
+                _eventTypes.Add(typeof(T));
+            }
+        }
+        //var mı yok mu listeye ekle basit bir method
+        private void AddSubscription(Type handlerType, string eventName)
+        {
+            if (!HasSubscriptionForEvent(eventName))
+            {
+                _handlers.Add(eventName, new List<SubscriptionInfo>());
+            }
+            if (_handlers[eventName].Any(s => s.HandlerType == handlerType))
+            {
+                throw new ArgumentException($"Handler Type {handlerType.Name} aldready registered for '{eventName}'", nameof(handlerType));
+            }
+            _handlers[eventName].Add(SubscriptionInfo.Typed(handlerType));
+        }
+
+        public void RemoveSubscription<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>
+        {
+            var handlerToRemove = FindSubscriptionToRemove<T, TH>();
+            var eventName = GetEventKey<T>();
+            RemoveHandler(eventName, handlerToRemove);
+        }
+
+        private void RemoveHandler(string eventName, SubscriptionInfo subsToRemove)
+        {
+            if (subsToRemove != null)
+            {
+                _handlers[eventName].Remove(subsToRemove);
+                if (!_handlers[eventName].Any())
+                {
+                    _handlers.Remove(eventName);
+                    var eventType = _eventTypes.SingleOrDefault(w => w.Name == eventName);
+                    if (eventType != null)
+                    {
+                        _eventTypes.Remove(eventType);
+                    }
+                    RaiseOnEventRemoved(eventName);
+                }
+            }
+        }
+        //event handler listesi geriye dönen
+        public IEnumerable<SubscriptionInfo> GetHandlerForEvent<T>() where T : IntegrationEvent
+        {
+            var key = GetEventKey<T>();
+            return GetHandlersForEvent(key);
+        }
+        public IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName) => _handlers[eventName];
+
+        //silinen eventler hakkında haber vermek amacıyla
+        private void RaiseOnEventRemoved(string eventName)
+        {
+            var handler = OnEventRemoved;
+            handler?.Invoke(this, eventName);
+        }
+        private SubscriptionInfo FindSubscriptionToRemove<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>
+        {
+            var eventName = GetEventKey<T>();
+            return FindSubscriptionToRemove(eventName, typeof(TH));
+        }
+        private SubscriptionInfo FindSubscriptionToRemove(string eventName, Type handlerType)
+        {
+            if (!HasSubscriptionForEvent(eventName))
+            {
+                return null!;
+            }
+            return _handlers[eventName].SingleOrDefault(w => w.HandlerType == handlerType)!;
+        }
+        public bool HasSubscriptionForEvent<T>() where T : IntegrationEvent
+        {
+            var key = GetEventKey<T>();
+            return HasSubscriptionForEvent(key);
+        }
+        public bool HasSubscriptionForEvent(string eventName) => _handlers.ContainsKey(eventName);
+
+        public Type GetEventTypeByName(string eventName) => _eventTypes.SingleOrDefault(w => w.Name == eventName)!;
+
+
+
+        public string GetEventKey<T>()
+        {
+            string eventName = typeof(T).Name;
+            return _eventNameGetter(eventName);
+        }
+
+    }
+}
---
+++ Src/BuildingBlocks/EventBus/EventBus.RabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,168 @@
+﻿using EventBus.Base;
+using EventBus.Base.Events;
+using Newtonsoft.Json;
+using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace EventBus.RabbitMQ
+{
+    public class EventBusRabbitMQ : BaseEventBus
+    {
+        RabbitMQPersistentConnection _persistentConnection;
+        private readonly IConnectionFactory _connectionFactory;
+        private readonly IModel _consumerChannel;
+
+        public EventBusRabbitMQ(EventBusConfig config, IServiceProvider serviceProvider) : base(config, serviceProvider)
+        {
+            if (config.Connection != null)
+            {
+                _connectionFactory = (ConnectionFactory)EventBusConfig.Connection;
+            }
+            else
+            {
+                _connectionFactory = new ConnectionFactory();
+            }
+            _persistentConnection = new RabbitMQPersistentConnection(_connectionFactory, config.ConnectionRetryCount);
+
+            _consumerChannel = CreateConsumerChannel();
+
+            SubsManager.OnEventRemoved += SubsManager_OnEventRemoved;
+        }
+
+        private void SubsManager_OnEventRemoved(object sender, string eventName)
+        {
+            eventName = ProcessEventName(eventName);
+            if (!_persistentConnection.IsConnected)
+            {
+                _persistentConnection.TryConnect();
+            }
+            _consumerChannel.QueueUnbind(queue: eventName,
+                exchange: EventBusConfig.DefaultTopicName,
+                routingKey: eventName);
+            if (SubsManager.IsEmpty)
+            {
+                _consumerChannel.Close();
+            }
+        }
+
+        public override void Publish(IntegrationEvent @event)
+        {
+            if (!_persistentConnection.IsConnected)
+            {
+                _persistentConnection.TryConnect();
+            }
+            var policy = Policy.Handle<BrokerUnreachableException>()
+                .Or<SocketException>()
+                .WaitAndRetry(EventBusConfig.ConnectionRetryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+                {
+
+
+                    //loglama yap
+
+                });
+
+            var eventName = @event.GetType().Name;
+            eventName = ProcessEventName(eventName);
+            //Ensure exchange exists while publishing
+            _consumerChannel.ExchangeDeclare(exchange: EventBusConfig.DefaultTopicName, type: "direct");
+
+
+
+            var message = JsonConvert.SerializeObject(@event);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            policy.Execute(() =>
+            {
+                var properties = _consumerChannel.CreateBasicProperties();
+                properties.DeliveryMode = 2;
+
+                _consumerChannel.BasicPublish(
+                    exchange: EventBusConfig.DefaultTopicName,
+                    routingKey: eventName,
+                    mandatory: true,
+                    basicProperties: properties,
+                    body: body);
+            });
+        }
+
+        public override void Subscribe<T, TH>()
+        {
+            var eventName = typeof(T).Name;
+            eventName = ProcessEventName(eventName);
+
+            if (!SubsManager.HasSubscriptionForEvent(eventName))
+            {
+                if (!_persistentConnection.IsConnected)
+                {
+                    _persistentConnection.TryConnect();
+                }
+                //emin olmak que consume için
+                _consumerChannel.QueueDeclare(queue: GetSubName(eventName),
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+                _consumerChannel.QueueBind(queue: GetSubName(eventName),
+                    exchange: EventBusConfig.DefaultTopicName,
+                    routingKey: eventName);
+            }
+
+            SubsManager.AddSubscription<T, TH>();
+            StartBasicConsume(eventName);
+
+        }
+
+        public override void UnSubscribe<T, TH>()
+        {
+            SubsManager.RemoveSubscription<T, TH>();
+        }
+
+        private IModel CreateConsumerChannel()
+        {
+            if (!_persistentConnection.IsConnected)
+            {
+                _persistentConnection.TryConnect();
+            }
+            var channel = _persistentConnection.CreateModel();
+            channel.ExchangeDeclare(exchange: EventBusConfig.DefaultTopicName,
+                type: "direct");
+            return channel;
+
+        }
+        private void StartBasicConsume(string eventName)
+        {
+            if (_consumerChannel != null)
+            {
+                var consumer = new EventingBasicConsumer(_consumerChannel);
+                consumer.Received += Consumer_Received;
+
+                _consumerChannel.BasicConsume(
+                    queue: GetSubName(eventName),
+                    autoAck: false,
+                    consumer: consumer);
+            }
+        }
+
+        private async void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var eventName = e.RoutingKey;
+            eventName = ProcessEventName(eventName);
+            var message = Encoding.UTF8.GetString(e.Body.Span);
+
+            try
+            {
+                await ProcessEvent(eventName, message);
+            }
+            catch (Exception)
+            {
+                //logging
+            }
+            _consumerChannel.BasicAck(e.DeliveryTag, multiple: false);
+        }
+    }
+}
