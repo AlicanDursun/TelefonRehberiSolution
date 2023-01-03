151cdeaf4856c97537603d8e7a908518d61bdf8b
AlicanDursun <alicangdursun@gmail.com>
2023-01-03 23:17:14 +0300

    Basic Tests Added

---
+++ Tests/BuildingBlocks/EventBus.UnitTest/EventBusTests.cs
@@ -0,0 +1,58 @@
+﻿using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.Factory;
+using EventBus.UnitTest.Events.EventHandlers;
+using EventBus.UnitTest.Events.Events;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+
+namespace EventBus.UnitTest
+{
+    public class EventBusTests
+    {
+        private ServiceCollection services;
+
+        public EventBusTests()
+        {
+            services = new ServiceCollection();
+            services.AddLogging(conf => conf.AddConsole());
+        }
+        [Test]
+        public void Subscribe_event_on_rabbitmq_test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+                return EventBusFactory.Create(GetRabbitMQConfig(), sp);
+            });
+            var sp = services.BuildServiceProvider();
+
+            var eventBus = sp.GetRequiredService<IEventBus>();
+            eventBus.Subscribe<ExcelRequestCreatedIntegrationEvent, ExcelRequestCreatedIntegrationEventHandler>();
+            eventBus.UnSubscribe<ExcelRequestCreatedIntegrationEvent, ExcelRequestCreatedIntegrationEventHandler>();
+        }
+        [Test]
+        public void Send_message_to_rabbitmq_test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+                return EventBusFactory.Create(GetRabbitMQConfig(), sp);
+            });
+            var sp = services.BuildServiceProvider();
+            var eventBus = sp.GetRequiredService<IEventBus>();
+
+            eventBus.Publish(new ExcelRequestCreatedIntegrationEvent(1));
+        }
+        private static EventBusConfig GetRabbitMQConfig()
+        {
+            return new EventBusConfig
+            {
+                ConnectionRetryCount = 5,
+                SubscriberClientAppName = "EventBus.UnitTest",
+                DefaultTopicName = "TelefonRehberiEventBus",
+                EventBusType = EventBusType.RabbitMQ,
+                EventNameSuffix = "IntegrationEvent"
+            };
+        }
+    }
+}
